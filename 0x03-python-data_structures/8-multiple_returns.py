0e774eb02cb1f27fe1d742335834896ffd0644ea
Hoot01 <hootamusaa2001@gmail.com>
2023-12-04 14:32:29 -0800

    lists,tuplas

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -18,5 +18,5 @@ def multiple_returns(sentence):
         a tuple with the length of a string and its first character
     """
 
-    str_len, first_char = len(sentence), sentence[0]
+    str_len, first_char = len(sentence), sentence[None]
     return (str_len, first_char)
