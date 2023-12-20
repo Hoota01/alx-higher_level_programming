e3d2ba2c7b9fc3c01c60b874b49f186318a26f52
Hoot01 <hootamusaa2001@gmail.com>
2023-12-19 17:21:32 -0800

    python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
+
 class Square:
-    """Defines a square"""
-    def __init__(self, size):
-        """Initialises the data"""
-        self.__size = size
+    """Class that defines properties of square.
+    """
+    pass
